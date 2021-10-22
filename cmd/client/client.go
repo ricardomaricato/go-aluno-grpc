1a3a44b0f8a560caa84c2d5f3bcefa24ce472709
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-10-21 22:44:05 -0300

    -Created client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/ricardomaricato/go-aluno-grpc/pb"
 	"google.golang.org/grpc"
@@ -19,8 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
-	// AddUsers(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 	// AddUserStreamBoth(client)
 
 }
@@ -61,4 +62,51 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "r1",
+			Name:  "Ricardo 1",
+			Email: "ricardo@ricardo.com",
+		},
+		&pb.User{
+			Id:    "r2",
+			Name:  "Ricardo 2",
+			Email: "ricardo@ricardo.com",
+		},
+		&pb.User{
+			Id:    "r3",
+			Name:  "Ricardo 3",
+			Email: "ricardo@ricardo.com",
+		},
+		&pb.User{
+			Id:    "r4",
+			Name:  "Ricardo 4",
+			Email: "ricardo@ricardo.com",
+		},
+		&pb.User{
+			Id:    "r2",
+			Name:  "Ricardo 5",
+			Email: "ricardo@ricardo.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
